d44cc7c7e1152b1a6ed847258896d0ef85a46955
sreenidhi2210 <68386630+sreenidhi2210@users.noreply.github.com>
2026-06-14 16:29:27 +0530

    Add files via upload

---
+++ Max_Profit_Problem.rb
@@ -0,0 +1,71 @@
+BUILDINGS = [
+  { name: "T", time: 5, earn: 1500 },
+  { name: "P", time: 4, earn: 1000 },
+  { name: "C", time: 10, earn: 2000 }
+]
+
+def optimize(n)
+  dp = Array.new(n + 1, -Float::INFINITY)
+  paths = Array.new(n + 1) { [] }
+
+  dp[0] = 0
+  paths[0] = [
+    { "T" => 0, "P" => 0, "C" => 0 }
+  ]
+
+  (0..n).each do |time_used|
+    next if dp[time_used] == -Float::INFINITY
+
+    BUILDINGS.each do |building|
+      new_time = time_used + building[:time]
+      next if new_time > n
+
+      profit =
+        building[:earn] * (n - new_time)
+
+      candidate_profit =
+        dp[time_used] + profit
+
+      new_paths =
+        paths[time_used].map do |path|
+          updated = path.dup
+          updated[building[:name]] += 1
+          updated
+        end
+
+      if candidate_profit > dp[new_time]
+        dp[new_time] = candidate_profit
+        paths[new_time] = new_paths
+
+      elsif candidate_profit == dp[new_time]
+        paths[new_time].concat(new_paths)
+      end
+    end
+  end
+
+  max_profit = dp.max
+
+  best_solutions = []
+
+  (0..n).each do |t|
+    next unless dp[t] == max_profit
+
+    best_solutions.concat(paths[t])
+  end
+
+  # Remove duplicates
+  best_solutions.uniq!
+
+  [max_profit, best_solutions]
+end
+
+n = gets.to_i
+
+profit, solutions = optimize(n)
+
+puts "Earnings: $#{profit}"
+puts "Solutions"
+
+solutions.each_with_index do |solution, index|
+  puts "#{index + 1}. T: #{solution['T']} P: #{solution['P']} C: #{solution['C']}"
+end
